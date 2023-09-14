1adfc7fcd8eaa9ddebd144da5de6935f1c369e95
GuGaTeC7 <gustavoluizsilva245@gmail.com>
2023-09-14 10:01:30 -0300

    update commit

--- js/index.js
+++ js/index.js
@@ -1,13 +1,11 @@
- // Capturando elementos HTML
+ // MODAL TIMES
  const escudo = document.getElementById('escudo');
  const divTimes = document.querySelector('.timesCompleto');
 
- // Adicionando um ouvinte de evento para quando o mouse entrar na imagem
  escudo.addEventListener('mouseenter', function() {
      divTimes.style.display = 'block';
  });
 
- // Adicionando um ouvinte de evento para quando o mouse sair da imagem
  escudo.addEventListener('mouseleave', function() {
      divTimes.style.display = 'none';
  });
