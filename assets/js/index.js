5a9bb278dbf31e45c776181dbf650cfc93a76956
GuGaTeC7 <gustavoluizsilva245@gmail.com>
2023-10-02 15:57:53 -0300

    update geral
    finalizado as validações login e cadastro. adicionado homeLogado

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,7 +11,7 @@ triangulo.addEventListener("mouseenter", function () {
   divTimes.style.display = "block";
 });
 
- escudo.addEventListener("mouseleave", function () {
+escudo.addEventListener("mouseleave", function () {
   divTimes.style.display = "none";
 });
 
@@ -76,8 +76,214 @@ function mudaCorE() {
 }
 
 // VALIDAÇÃO LOGIN
+document.getElementById('loginForm').addEventListener('submit', function (e) {
+  e.preventDefault(); // Impede o envio padrão do formulário
+});
+
+function logar() {
+var login = document.getElementById('nome').value;
+var senha = document.getElementById('senha').value;
+
+var usuariosCadastrados = ['admin', 'gustavo'];
+var senhasCadastradas = ['admin', 'gustavo'];
+
+// Verifica se tanto o nome de usuário quanto a senha estão nas respectivas listas
+if (usuariosCadastrados.includes(login) && senhasCadastradas.includes(senha)) {
+    bootbox.alert({
+        message: 'Login realizado com sucesso!',
+        size: 'small',
+        buttons: {
+            ok: {
+                className: 'btn btn-success'
+            }
+        },
+        callback: function () {
+            window.location.href = 'http://127.0.0.1:5501/view/homeLogado.html';
+        }
+    });
+    } else if (login === '' || senha === '') {
+        bootbox.alert({
+            message: 'Insira seu <b>Nome de usuário</b> e <b>Senha</b> para enviar!',
+            size: 'large',
+            buttons: {
+                ok: {
+                    className: 'btn btn-success'
+                }
+            }
+        });
+    }
+    else {
+        bootbox.alert({
+            message: 'Nome de usuário ou senha inválidos!',
+            size: 'small',
+            buttons: {
+                ok: {
+                    className: 'btn btn-success'
+                }
+            }
+        });
+    }
+}
+
+// REVELA SENHA
+ let olho = document.getElementById('olho');
+ let senha = document.getElementById('senha');
+
+
+ olho.onclick = function() {
+  if(senha.type == 'password') {
+    senha.type = 'text';
+    olho.src = '/assets/img/icons8-invisível-20.png';
+  } else {
+    senha.type = 'password';
+    olho.src = '/assets/img/icons8-visível-20.png';
+  }
+ }
+
+
 
 // VALIDAÇÃO EMAIL
+function validaCadastro() {
+  const nome = document.getElementById('nome').value;
+  const nomeUsuario = document.getElementById('nomeUsuario').value;
+  const email = document.getElementById('email').value;
+  const telefone = document.getElementById('telefone').value;
+  const senha = document.getElementById('senha').value;
+  const senhaC = document.getElementById('senhaC').value;
+  const sexo = document.querySelector('input[name="gender"]:checked');
+  const termos = document.getElementById('termos');
 
+  if (!nome) {
+    bootbox.alert({
+    message: 'Insira seu <b>Nome Completo</b>!',
+    size: 'small',
+    buttons: {
+        ok: {
+            className: 'btn btn-success'
+        }
+    }
+  });
+    return;
+  }
+
+  if (!nomeUsuario) {
+    bootbox.alert({
+    message: 'Insira seu <b>Nome de Usuário</b>!',
+    size: 'small',
+    buttons: {
+        ok: {
+            className: 'btn btn-success'
+        }
+    }
+  });
+    return;
+  }
+
+  if (!email) {
+    bootbox.alert({
+    message: 'Insira seu <b>Email</b>!',
+    size: 'small',
+    buttons: {
+        ok: {
+            className: 'btn btn-success'
+        }
+    }
+  });
+    return;
+  }
+
+  if (!telefone) {
+    bootbox.alert({
+    message: 'Insira seu <b>Telefone</b>!',
+    size: 'small',
+    buttons: {
+        ok: {
+            className: 'btn btn-success'
+        }
+    }
+  });
+    return;
+  }
+
+  if (!senha) {
+    bootbox.alert({
+    message: 'Insira uma <b>Senha</b>!',
+    size: 'small',
+    buttons: {
+        ok: {
+            className: 'btn btn-success'
+        }
+    }
+  });
+    return;
+  }
+
+  if (!senhaC) {
+    bootbox.alert({
+    message: 'Confirme sua <b>Senha</b>!',
+    size: 'small',
+    buttons: {
+        ok: {
+            className: 'btn btn-success'
+        }
+    }
+  });
+    return;
+  }
+
+  if (!sexo) {
+    bootbox.alert({
+    message: 'Insira seu <b>Sexo Biológico</b>!',
+    size: 'small',
+    buttons: {
+        ok: {
+            className: 'btn btn-success'
+        }
+    }
+  });
+    return;
+  }
+  if (!termos.checked) {
+    bootbox.alert({
+    message: 'Aceite os <b>Termos de uso</b> e <b>Política de Privacidade</b>!',
+    size: 'large',
+    buttons: {
+        ok: {
+            className: 'btn btn-success'
+        }
+    }
+  });
+    return;
+  }
+
+
+  // VALIDA SENHA
+  if (senha !== senhaC) {
+    bootbox.alert({
+    message: 'Senhas não coincidem!',
+    size: 'small',
+    buttons: {
+        ok: {
+            className: 'btn btn-success'
+        }
+    }
+  });
+    return;
+  } else {
+    bootbox.alert({
+    message: 'Cadastro realizado com sucesso!',
+    size: 'small',
+    buttons: {
+        ok: {
+            className: 'btn btn-success'
+        }
+    },
+    callback: function () {
+        window.location.href = 'http://127.0.0.1:5501/view/homeLogado.html';
+    }
+  });
+  }
+
+}
 
   
